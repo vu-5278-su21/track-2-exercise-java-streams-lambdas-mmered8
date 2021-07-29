ec0b9ceda5b4fd61af11e6a1221093c608eefca1
Mason Meredith <mason.mere@gmail.com>
2021-07-28 19:43:47 -0500

    working

--- src/main/java/edu/vanderbilt/cs/streams/BikeStats.java
+++ src/main/java/edu/vanderbilt/cs/streams/BikeStats.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.OptionalDouble;
+import java.util.UUID;
 import java.util.function.Function;
 import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
@@ -48,37 +49,26 @@ public class BikeStats {
      * @return
      */
     public Stream<BikeRide.DataFrame> averagedDataFrameStream(int windowSize){
+// Broken partial solution, wrong methods
+//    	Stream<BikeRide.DataFrame> initialStream = ride.fusedFramesStream();
+//    	List<BikeRide.DataFrame> bikeList = initialStream.collect(Collectors.toList());
+//    	int sizeDividedList = (bikeList.size()/windowSize) -1;
+//    	
+//        Stream<List<BikeRide.DataFrame>> slideWindow = StreamUtils.slidingWindow(bikeList, sizeDividedList);
+//    	Iterator<List<DataFrame>> it = slideWindow.iterator();
+//    	BikeRide.DataFrame[] arrayHolder = new BikeRide.DataFrame[sizeDividedList];
+//    	arrayHolder = it.next().toArray(new DataFrame[sizeDividedList]);;
+
     	Stream<BikeRide.DataFrame> initialStream = ride.fusedFramesStream();
     	List<BikeRide.DataFrame> bikeList = initialStream.collect(Collectors.toList());
-//    	Stream<List<BikeRide.DataFrame>> slideWindow = StreamUtils.slidingWindow(bikeList, windowSize);
-//    	DataFrame[] bikeArray = bikeList.toArray(new DataFrame[bikeList.size()]);
-    	
-    	int size = (bikeList.size()/windowSize) -1;
-    	
-//    	if(windowSize > bikeList.size()) 
-//            return Stream.empty();
-//    	return slideWindow.to;// map(start -> bikeList.subList(0, windowSize));
-//    	return slideWindow.map(start -> bikeList.subList(0, windowSize));
-    	
-//    	if(windowSize > bikeList.size()) 
-//            return Stream.empty();
-//        return IntStream.range(0, bikeList.size()-windowSize+1)
-//                        .mapToObj(start -> bikeList.subList(start, start+windowSize));
-    	
-//    	Stream<BikeRide.DataFrame> initialStream = ride.fusedFramesStream(); 
-//    	List<BikeRide.DataFrame> bikeList = initialStream.collect(Collectors.toList());
-//    	
-    	Stream<List<BikeRide.DataFrame>> slideWindow = StreamUtils.slidingWindow(bikeList, size);
-    	
-    	Iterator<List<DataFrame>> it = slideWindow.iterator();
-    	
-    	BikeRide.DataFrame[] arrayHolder = new BikeRide.DataFrame[size];
-    	
-//    	for(int i=0; i<size && it.hasNext(); i++) {
-    		arrayHolder = it.next().toArray(new DataFrame[size]);
-//    	}
-//    	arrayHolder[0] = bikeList.get(0);
-        return Arrays.stream(arrayHolder);
+    	Stream<List<BikeRide.DataFrame>> slideWindow = StreamUtils.slidingWindow(bikeList, windowSize);
+
+    	return slideWindow.map(i -> new BikeRide.DataFrame(i.get(0).coordinate, 
+    					StreamUtils.averageOfProperty(BikeRide.DataFrame::getGrade).apply(i),
+    					StreamUtils.averageOfProperty(BikeRide.DataFrame::getAltitude).apply(i), 
+    					StreamUtils.averageOfProperty(BikeRide.DataFrame::getVelocity).apply(i), 
+    					StreamUtils.averageOfProperty(BikeRide.DataFrame::getHeartRate).apply(i))
+    				);
     }
 
     // @ToDo:
